5bfaa2b9e6d7dc618a47124ffdf26e24996faebf
Minh Than <hoangminh117@gmail.com>
2025-06-11 08:55:39 +0700

    chore(central_panel): move special edge-case handling to inverse pole set

--- src/models/inverse_pole_set/mod.rs
+++ src/models/inverse_pole_set/mod.rs
@@ -1,3 +1,5 @@
+use std::iter::zip;
+
 use crate::{models::circle::Circle, models::segment::Segment, services};
 use egui::Pos2;
 
@@ -22,29 +24,33 @@ impl InversePoleSet {
         let p2 = services::calc::get_inverse_pole(&line, circles[1]);
         let p3 = services::calc::get_inverse_pole(&line, circles[2]);
 
-        let point_segment_pairs: Vec<PointSegmentPair> = vec![
-            PointSegmentPair(
-                p1,
+        let mut point_segment_pairs: Vec<PointSegmentPair> = Vec::new();
+        for (circle, p) in zip(circles, [p1, p2, p3]) {
+            point_segment_pairs.push(PointSegmentPair(
+                p,
                 services::calc::get_circle_straight_line_intersection(
-                    &Some(Segment(p1?, radical_center).as_straight_line()),
-                    &circles[0],
+                    &Some(Segment(p?, radical_center).as_straight_line()),
+                    &circle,
                 ),
-            ),
-            PointSegmentPair(
-                p2,
+            ));
+        }
+
+        Some(InversePoleSet {
+            point_segment_pairs,
+        })
+    }
+
+    pub fn new_special(circles: &[Circle], radical_center: Pos2) -> Option<Self> {
+        let mut point_segment_pairs: Vec<PointSegmentPair> = Vec::new();
+        for circle in circles {
+            point_segment_pairs.push(PointSegmentPair(
+                Some(circle.center),
                 services::calc::get_circle_straight_line_intersection(
-                    &Some(Segment(p2?, radical_center).as_straight_line()),
-                    &circles[1],
+                    &Some(Segment(circle.center, radical_center).as_straight_line()),
+                    circle,
                 ),
-            ),
-            PointSegmentPair(
-                p3,
-                services::calc::get_circle_straight_line_intersection(
-                    &Some(Segment(p3?, radical_center).as_straight_line()),
-                    &circles[2],
-                ),
-            ),
-        ];
+            ));
+        }
 
         Some(InversePoleSet {
             point_segment_pairs,
