0a498b5c3fc46eeb7128096239829e50abd5158b
Minh Than <hoangminh117@gmail.com>
2025-06-26 09:18:39 +0700

    chore(homothetic_set): refactor

--- src/models/homothetic_set/mod.rs
+++ src/models/homothetic_set/mod.rs
@@ -10,6 +10,14 @@ pub struct HomotheticPair {
     pub ex: Option<Pos2>,
     pub ir: Option<Pos2>,
 }
+impl HomotheticPair {
+    pub fn new(circle_1: Circle, circle_2: Circle) -> Self {
+        Self {
+            ex: services::calc::get_external_homothetic_center(circle_1, circle_2),
+            ir: services::calc::get_internal_homothetic_center(circle_1, circle_2),
+        }
+    }
+}
 
 #[derive(Clone)]
 pub struct HomotheticSet {
@@ -18,35 +26,19 @@ pub struct HomotheticSet {
 }
 impl HomotheticSet {
     pub fn new(circles: &[Circle]) -> Self {
-        let ex_12 = services::calc::get_external_homothetic_center(circles[0], circles[1]);
-        let ir_12 = services::calc::get_internal_homothetic_center(circles[0], circles[1]);
-        let ex_23 = services::calc::get_external_homothetic_center(circles[1], circles[2]);
-        let ir_23 = services::calc::get_internal_homothetic_center(circles[1], circles[2]);
-        let ex_31 = services::calc::get_external_homothetic_center(circles[2], circles[0]);
-        let ir_31 = services::calc::get_internal_homothetic_center(circles[2], circles[0]);
+        let pairs: [HomotheticPair; 3] = [
+            HomotheticPair::new(circles[0], circles[1]),
+            HomotheticPair::new(circles[1], circles[2]),
+            HomotheticPair::new(circles[2], circles[0]),
+        ];
 
-        let line_1 = Segment::get_any_valid_segment(vec![ex_31, ex_23, ex_12]);
-        let line_2 = Segment::get_any_valid_segment(vec![ex_12, ir_31, ir_23]);
-        let line_3 = Segment::get_any_valid_segment(vec![ex_31, ir_23, ir_12]);
-        let line_4 = Segment::get_any_valid_segment(vec![ex_23, ir_31, ir_12]);
+        let lines: [Option<Segment>; 4] = [
+            Segment::get_any_valid_segment(vec![pairs[2].ex, pairs[1].ex, pairs[0].ex]),
+            Segment::get_any_valid_segment(vec![pairs[0].ex, pairs[2].ir, pairs[1].ir]),
+            Segment::get_any_valid_segment(vec![pairs[2].ex, pairs[1].ir, pairs[0].ir]),
+            Segment::get_any_valid_segment(vec![pairs[1].ex, pairs[2].ir, pairs[0].ir]),
+        ];
 
-        Self {
-            pairs: [
-                HomotheticPair {
-                    ex: ex_12,
-                    ir: ir_12,
-                },
-                HomotheticPair {
-                    ex: ex_23,
-                    ir: ir_23,
-                },
-                HomotheticPair {
-                    ex: ex_31,
-                    ir: ir_31,
-                },
-            ],
-            lines: [line_1, line_2, line_3, line_4],
-        }
+        Self { pairs, lines }
     }
 }
-
